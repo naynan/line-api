fe740a488902c2a55ff1d9b1a813ff1986adf5bc
naynan <suwits@fbbattery.com>
2019-09-03 11:30:08 +0700

    Update sendtex.php

--- sendtex.php
+++ sendtex.php
@@ -1,6 +1,6 @@
 <?php
 require "vendor/autoload.php";
-$access_token = 'bHg3caMXHTfsUJ3Psg+sfwwUvB00ZsCehpopYCGrAuufZDrT+WZ9/a8XWrEJcRpdGkOE5m+Bu2P1FJRbx4D3YZ69+AOvXkOcrt1qZ40dCMU9h5ZT9sH43Kxrk/3deMfW2XU4AAIAgqfUkRreo527vgdB04t89/1O/w1cDnyilFU=';
+$access_token = 'G0zVo2dcDP+YOM6jDvgdn+19a868zOZTDC2JWkFtlYY6V/Nokm6fA0jtKxpwDIbhGkOE5m+Bu2P1FJRbx4D3YZ69+AOvXkOcrt1qZ40dCMVulgygIcxMbSZVRfVeH6F9jSHY+J6X8RqA955B2zSOdgdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '45e49a645492d34615a11abb5a68e30b';
 $idPush = 'Ub9416c2f0b262e29f616e789841c4ff7'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
